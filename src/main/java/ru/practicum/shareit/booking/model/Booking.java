c662546b66749125228efaaa9762207bcc62939f
dvoe4ka <dvoooe4ka@yandex.ru>
2023-07-03 19:12:28 +0300

    start

--- src/main/java/ru/practicum/shareit/booking/model/Booking.java
+++ src/main/java/ru/practicum/shareit/booking/model/Booking.java
@@ -2,10 +2,15 @@ package ru.practicum.shareit.booking.model;
 
 import lombok.Data;
 
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Data
+@Entity
+@Table(name = "booking")
 public class Booking {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
     private LocalDateTime start;
     private LocalDateTime end;
