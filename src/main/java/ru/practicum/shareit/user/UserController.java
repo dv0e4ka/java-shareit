fa5da0050c3d30a292416bd00589b36f396162aa
dvoe4ka <dvoooe4ka@yandex.ru>
2023-07-17 03:39:04 +0300

    feat: add test for userController

--- src/main/java/ru/practicum/shareit/user/UserController.java
+++ src/main/java/ru/practicum/shareit/user/UserController.java
@@ -2,6 +2,7 @@ package ru.practicum.shareit.user;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -15,6 +16,7 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public UserDto add(@Valid @RequestBody UserDto user) {
         log.info("получен запрос на добавление пользователя с именем={}, и email={}", user.getName(), user.getEmail());
         return userService.add(user);
--- src/test/java/ru/practicum/shareit/user/UserControllerTest.java
+++ src/test/java/ru/practicum/shareit/user/UserControllerTest.java
@@ -1,16 +1,24 @@
 package ru.practicum.shareit.user;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import ru.practicum.shareit.item.ItemService;
+import ru.practicum.shareit.util.Header;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(controllers = UserController.class)
 class UserControllerTest {
@@ -26,102 +34,97 @@ class UserControllerTest {
     @MockBean
     private UserService userService;
 
+    UserDto userDtoIn;
+    UserDto userDtoOut;
+
     @BeforeEach
     void setUp() {
+        userDtoIn = UserDto.builder()
+                .name("name")
+                .email("name@mail.ru")
+                .build();
+        userDtoOut = UserDto.builder()
+                .id(1L)
+                .name("name")
+                .email("name@mail.ru")
+                .build();
     }
 
     @Test
-    void q1() throws Exception {
+    void shouldSave() throws Exception {
+        when(userService.add(userDtoIn)).thenReturn(userDtoOut);
 
+        mvc.perform(post(URL)
+                        .content(mapper.writeValueAsString(userDtoIn))
+                        .characterEncoding(StandardCharsets.UTF_8)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isCreated())
+                .andExpect(content().json(mapper.writeValueAsString(userDtoOut)));
     }
 
     @Test
-    void q2() throws Exception {
-
+    void shouldSave_whenNameIsEmpty_fail() throws Exception {
+        userDtoIn.setName("");
+        mvc.perform(post(URL)
+                        .content(mapper.writeValueAsString(userDtoIn))
+                        .characterEncoding(StandardCharsets.UTF_8)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isBadRequest());
     }
 
     @Test
-    void q3() throws Exception {
-
+    void shouldSave_whenEmailNotValid_fail() throws Exception {
+        userDtoIn.setEmail("");
+        mvc.perform(post(URL)
+                        .content(mapper.writeValueAsString(userDtoIn))
+                        .characterEncoding(StandardCharsets.UTF_8)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isBadRequest());
     }
 
     @Test
-    void q4() throws Exception {
+    void shouldUpdate() throws Exception {
+        when(userService.patch(1L, userDtoIn)).thenReturn(userDtoOut);
 
+        mvc.perform(patch(URL + "/1")
+                        .content(mapper.writeValueAsString(userDtoIn))
+                        .characterEncoding(StandardCharsets.UTF_8)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .header(Header.X_SHARED_USER_ID, 1))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().json(mapper.writeValueAsString(userDtoOut)));
     }
 
     @Test
-    void q5() throws Exception {
+    void findById() throws Exception {
+        when(userService.findById(1L)).thenReturn(userDtoOut);
 
+        mvc.perform(get(URL + "/1"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().json(mapper.writeValueAsString(userDtoOut)));
     }
 
     @Test
-    void q6() throws Exception {
+    void findAll() throws Exception {
+        List<UserDto> userDtoList = List.of(userDtoOut);
 
+        when(userService.getAll()).thenReturn(userDtoList);
+
+        mvc.perform(get(URL))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().json(mapper.writeValueAsString(userDtoList)));
     }
 
     @Test
-    void q7() throws Exception {
-
-    }
-
-    @Test
-    void q8() throws Exception {
-
-    }
-
-    @Test
-    void q9() throws Exception {
-
-    }
-
-    @Test
-    void q10() throws Exception {
-
-    }
-
-    @Test
-    void q11() throws Exception {
-
-    }
-
-    @Test
-    void q12() throws Exception {
-
-    }
-
-    @Test
-    void q13() throws Exception {
-
-    }
-
-    @Test
-    void q14() throws Exception {
-
-    }
-
-    @Test
-    void q15() throws Exception {
-
-    }
-
-    @Test
-    void q16() throws Exception {
-
-    }
-
-    @Test
-    void q17() throws Exception {
-
-    }
-
-    @Test
-    void q18() throws Exception {
-
-    }
-
-    @Test
-    void q19() throws Exception {
-
+    void shouldDelete() throws Exception {
+        mvc.perform(delete(URL + "/1"))
+                .andDo(print())
+                .andExpect(status().isOk());
     }
 }
