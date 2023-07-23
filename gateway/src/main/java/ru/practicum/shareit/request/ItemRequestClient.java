8f4fc5ab156cb292414be36355e7e55d3156eaa3
dvoe4ka <dvoooe4ka@yandex.ru>
2023-07-23 03:17:12 +0300

    feat:добавил request в gateway

---
+++ gateway/src/main/java/ru/practicum/shareit/request/ItemRequestClient.java
@@ -0,0 +1,42 @@
+package ru.practicum.shareit.request;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.stereotype.Service;
+import org.springframework.web.util.DefaultUriBuilderFactory;
+import ru.practicum.shareit.client.BaseClient;
+
+import java.util.Map;
+
+@Service
+public class ItemRequestClient extends BaseClient {
+    private static String API_PREFIX = "/requests";
+
+    public ItemRequestClient(@Value("http://localhost:9090") String serverUrl, RestTemplateBuilder builder) {
+        super(
+                builder.uriTemplateHandler(new DefaultUriBuilderFactory(serverUrl + API_PREFIX))
+                        .requestFactory(HttpComponentsClientHttpRequestFactory::new)
+                        .build()
+        );
+    }
+
+    public ResponseEntity<Object> save(long userId, ItemRequestDto itemRequestDto) {
+        return post("", itemRequestDto);
+    }
+
+    public ResponseEntity<Object> getById(long userId, long requestId) {
+        return get("/" + requestId, userId);
+    }
+
+    public ResponseEntity<Object> getAllByUser(long userId) {
+        return get("", userId);
+    }
+
+    public ResponseEntity<Object> getAll(long userId, int from, int size) {
+        Map<String, Object> parameters = Map.of("from", from,
+                "size", size);
+        return get("/all", userId, parameters);
+    }
+}
